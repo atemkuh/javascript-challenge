df3eb075564c212921a37fd0b086dab8774761b9
joseph <>
2020-12-14 07:55:44 -0500

    create console log, and select tbody, append each tro to a ufo value

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,14 @@
 // from data.js
 var tableData = data;
+// create a console.log
+console.log(tableData)
 
+// YOUR CODE HERE!
+// use select syntax to ref table body
+var tbody=d3.select("tbody");
+
+//use forEach meth0d for UFO values in each column
+tableData.forEach(function(ufoSighting) {
+    console.log(ufoSighting);
+    var row=tbody.append("tr");// append each tble row to each UFO value
 // YOUR CODE HERE!
