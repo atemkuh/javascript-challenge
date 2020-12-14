9f58e1080c7b25763e73762b5936096346a62928
joseph <>
2020-12-14 08:11:48 -0500

    append append each value to a row

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -11,4 +11,15 @@ var tbody=d3.select("tbody");
 tableData.forEach(function(ufoSighting) {
     console.log(ufoSighting);
     var row=tbody.append("tr");// append each tble row to each UFO value
-// YOUR CODE HERE!
+    
+
+    // for each ufo value in console.log use "Object.entries"
+    Object.entries(ufoSighting).forEach(function([key,value]) {
+        console.log(key,value);
+        var cell =row.append("td");// append each value to a row
+        cell.text(value);
+
+    })
+   
+})
+
