af2d148712a15e69912504dcdd56f7b696b8acf8
joseph <>
2020-12-15 08:15:16 -0500

    correct html loading errors on app.js scripts

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -11,7 +11,7 @@ var tbody=d3.select("tbody");
 tableData.forEach(function(ufoSighting) {
     console.log(ufoSighting);
     var row=tbody.append("tr");// append each tble row to each UFO value
-    
+
 
     // for each ufo value in console.log use "Object.entries"
     Object.entries(ufoSighting).forEach(function([key,value]) {
@@ -19,9 +19,9 @@ tableData.forEach(function(ufoSighting) {
         var cell =row.append("td");// append each value to a row
         cell.text(value);
 
-    })
-   
-})
+    });
+
+});
 // filter button selection
 var button =d3.select("#filter-btn");
 button.on("click", function(){
@@ -40,9 +40,9 @@ button.on("click", function(){
         cell.text(value);
 
 
-    })
+    });
 
 
 
-    })
-})
+  });
+});
